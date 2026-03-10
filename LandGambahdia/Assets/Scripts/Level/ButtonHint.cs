4d79e8edf5417dad6a43e3f58849f0d56a8a7e4c
alex23job <106354657+alex23job@users.noreply.github.com>
2026-03-11 02:29:34 +0300

    Update for select building viewing hint

--- LandGambahdia/Assets/Scripts/Level/ButtonHint.cs
+++ LandGambahdia/Assets/Scripts/Level/ButtonHint.cs
@@ -15,12 +15,14 @@ public class ButtonHint : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     {        
         RectTransform rectHint = _hintPanel.GetComponent<RectTransform>();
         RectTransform rectButton = gameObject.GetComponent<RectTransform>();
-        Vector3 hintPos = new Vector3(transform.position.x + rectButton.rect.width / 2, transform.position.y + rectButton.rect.height / 2, _hintPanel.transform.position.z);
+        //Vector3 hintPos = new Vector3(transform.position.x + rectButton.rect.width / 2, transform.position.y + rectButton.rect.height / 2, _hintPanel.transform.position.z);
+        Vector3 hintPos = new Vector3(transform.position.x + rectButton.rect.width / 3, transform.position.y + rectButton.rect.height * 0.9f, _hintPanel.transform.position.z);
         _hintPanel.transform.position = hintPos;
         _txtHint.text = _hintText;
 
-        print($"posBtn={transform.position} (w={rectButton.rect.width}, h={rectButton.rect.height})  posHint={hintPos} (w={rectHint.rect.width}, h={rectHint.rect.height})");
+        //print($"posBtn={transform.position} (w={rectButton.rect.width}, h={rectButton.rect.height})  posHint={hintPos} (w={rectHint.rect.width}, h={rectHint.rect.height})");
         _hintPanel.SetActive(true);
+        
     }
 
     public void OnPointerExit(PointerEventData eventData)
